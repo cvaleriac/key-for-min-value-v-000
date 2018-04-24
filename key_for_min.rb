89e213001eab8b238d91286be773c1e1ae22c955
valeria campilongo <cvaleriac@hotmail.it>
2018-04-24 15:35:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   smallest_key = nil
-    tiniest_value = nil
+    smallest_value = nil
     name_hash.each do |key, value|
-      if tiniest_value == nil || value < tiniest_value
-        tiniest_value = value
+      if smallest_value == nil || value < smallest_value
+        smallest_value = value
         smallest_key = key
       end
     end
